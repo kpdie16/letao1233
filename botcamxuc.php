36a5ea6ef6182782d664a14457b38ce0375ecf5d
kpdie16 <sabovsle@gmail.com>
2017-02-07 14:18:21 +0700

    Add files via upload

---
+++ botcamxuc.php
@@ -0,0 +1,22 @@
+﻿<?php
+$token = 'Tokencuaban?'; //Token Lấy Ở Phương Bách
+$limitnf=20; // 20 Status Ở NewFeed 1 Lần
+$puaru=json_decode(puaru('https://graph.facebook.com/me/home?fields=id,message,created_time,from,comments,type&access_token='.$token.'&offset=0&limit='.$limitnf.''),true);
+for($i=1;$i<=count($puaru[data]);$i++){
+set_time_limit(0);
+$camxuc= array('LOVE');
+$mess=$camxuc[rand(0,count($camxuc)-1)];
+echo puaru('https://graph.facebook.com/'.$puaru[data][$i-1][id].'/reactions?type='.$mess.'&method=post&access_token='.$token.'');
+}
+
+//**Hãy Tôn Trọng Tác Giả Nếu Bạn Muốn Có Nhiều Sản Phẩm Miễn Phí Khác Để Dùng**//
+//** Code By Puaru - Vina4U.BIZ**//
+function puaru($url){
+$data = curl_init();
+curl_setopt($data, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($data, CURLOPT_URL, $url);
+$hasil = curl_exec($data);
+curl_close($data);
+return $hasil;
+}
+?>
